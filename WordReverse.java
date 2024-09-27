43065e3ec0e577e886ae7acaef456746f4cfc7e9
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2024-09-27 12:44:47 +0530

    Add files via upload

---
+++ WordReverse.java
@@ -0,0 +1,20 @@
+package String;
+
+public class WordReverse {
+
+	public static void main(String[] args) {
+		
+		String input = "this is pen";
+        
+        // Split the input string into words
+        String[] words = input.split(" ");
+        
+        // Rearrange the words
+        String rearranged = words[2] + " " + words[1] + " " + words[0];
+        
+        // Output the result
+        System.out.println(rearranged);
+
+	}
+
+}
