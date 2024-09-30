582c57b2ce64a7d4815d97504182984a324ca5b9
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2024-09-30 16:21:19 +0530

    Add files via upload

---
+++ WithoutUsingSelectClass.java
@@ -0,0 +1,31 @@
+package SelectClass;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WithoutUsingSelectClass {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\91620\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/signup");
+		driver.manage().window().maximize();
+		List<WebElement> Daylist=driver.findElements(By.xpath("//*[@name='birthday_day']//option"));
+		System.out.println(Daylist.size());
+		
+		for(int i=0;i<Daylist.size();i++)
+		{
+			System.out.println(Daylist.get(i).getText());
+			if(Daylist.get(i).getText().equals("56")) {
+				Daylist.get(i).click();
+				break;
+			}
+		}	
+
+	}
+
+}
