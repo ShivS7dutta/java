bcdddca3ce7479b2e2502aa84a1ddf051d069a0c
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2024-12-02 15:33:01 +0530

    Add files via upload

---
+++ CountOccrance.java
@@ -0,0 +1,68 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountOccrance {
+
+	public static void main(String[] args) {
+
+		//Scanner scanner = new Scanner(System.in);
+		//  System.out.print("Enter a string: ");
+		//  String input = scanner.nextLine();
+		String input="shivnath3453";
+
+		Map<Character, Integer> charCountMap = new HashMap<>();
+
+		for (char c : input.toCharArray()) {
+			charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+		}
+
+		System.out.println("Character occurrences:");
+		for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
+
+		//scanner.close();
+		/*
+		String str = "google"; 
+		HashMap <Character, Integer> charCount = new HashMap<>();  
+		for (int i = str.length() - 1; i >= 0; i--)   
+		{  
+		if(charCount.containsKey(str.charAt(i)))   
+		{  
+		int count = charCount.get(str.charAt(i));  
+		charCount.put(str.charAt(i), ++count);  
+		}   
+		else   
+		{  
+		charCount.put(str.charAt(i),1);  
+		}  
+		}  
+		System.out.println(charCount); */
+		/*
+		       String str="Deepak";
+char ch[]=str.toCharArray();
+int count=0;
+for(int i=0;i<ch.length;i++)
+{
+	for(int j=0;j<ch.length;j++)
+	{
+	  if(ch[i]==ch[j] && j<i)
+	  {
+	    break;
+	   }
+	  if(ch[i]==ch[j])
+	  {
+           count++;
+           }
+	}
+	if(count>0)
+	{
+	System.out.println(ch[i]+" "+count);
+	}
+}
+		 */
+
+	}}
