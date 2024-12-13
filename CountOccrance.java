6edcd72073c8405622f34d9211686668881ed877
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2024-12-13 17:10:01 +0530

    Add files via upload

---
+++ CountOccrance.java
@@ -0,0 +1,40 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountOccrance {
+
+	public static void main(String[] args) {
+
+		//Scanner scanner = new Scanner(System.in);
+		//  System.out.print("Enter a string: ");
+		//  String input = scanner.nextLine();
+	
+		/*
+		String input = "google test"; 
+        HashMap<Character, Integer> charCount = new HashMap<>();
+        
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch != ' ') {  // Skip spaces
+                charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+            }
+        }
+
+        System.out.println(charCount);*/
+		
+/*
+String str1 = "DevLabs Alliance is awesome.";
+char ch1 = 'e';
+int frequency = 0;
+
+for(int i = 0; i < str.length(); i++) {
+  if(ch1 == str.charAt(i)) {
+     ++frequency;
+     }
+  }
+System.out.println("Frequency of " + ch + " = " + frequency); 		
+*/
+}}
