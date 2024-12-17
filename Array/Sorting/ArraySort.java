f05e361c0b7adae53d4f125bf39ae970e1453e8d
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2024-12-17 18:33:46 +0530

    Add files via upload

---
+++ Array/Sorting/ArraySort.java
@@ -0,0 +1,19 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class ArraySort {
+
+	public static void main(String[] args) {
+		int[] arr={2,9,3,5,58,4,7,1,8,6};
+       // int temp;
+       Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+
+	}
+
+}
