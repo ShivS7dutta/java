3392740e6625671478c46da88207af529216e2ee
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2024-12-21 09:07:26 +0530

    Add files via upload

---
+++ CountOccrance.java
@@ -0,0 +1,43 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountOccrance {
+
+	public static void main(String[] args) {
+
+		//Scanner scanner = new Scanner(System.in);
+		//  System.out.print("Enter a string: ");
+		//  String input = scanner.nextLine();
+	
+		/*
+		String input = "google test"; 
+        HashMap<Character, Integer> charCount = new HashMap<>();
+Iterate over the string: The loop goes from the start (i = 0) to the end of the string.        
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch != ' ') {  // Skip spaces
+                charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+            }
+        }
+
+        System.out.println(charCount);
+getOrDefault(): This method is used to check if the character is already in the map. 
+If it is, we increment its count; otherwise, we set its count to 1.       
+        */
+		
+/*
+String str1 = "DevLabs Alliance is awesome.";
+char ch1 = 'e';
+int frequency = 0;
+
+for(int i = 0; i < str.length(); i++) {
+  if(ch1 == str.charAt(i)) {
+     ++frequency;
+     }
+  }
+System.out.println("Frequency of " + ch + " = " + frequency); 		
+*/
+}}
