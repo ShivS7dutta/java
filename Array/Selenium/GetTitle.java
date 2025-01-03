981b427e94196b61b57048a6e679db17f5ba2211
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2025-01-03 12:20:01 +0530

    Add files via upload

---
+++ Array/Selenium/GetTitle.java
@@ -0,0 +1,25 @@
+package AssertTestng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GetTitle {
+	
+	@Test
+	public void testTitle()
+	{
+		
+		String expectedtitle="Electronics, Cars, Fashion, Collectibles & More | eBay";
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\91620\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.ebay.com/");
+		driver.manage().window().maximize();
+		String actualtitle=driver.getTitle();
+		
+		Assert.assertEquals(actualtitle, expectedtitle);
+		
+	}
+
+}
