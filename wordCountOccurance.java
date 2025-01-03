2b8689a87b139681a473690506520803f7b1a0e3
ShivS7dutta <139039334+ShivS7dutta@users.noreply.github.com>
2025-01-03 12:16:56 +0530

    Add files via upload

---
+++ wordCountOccurance.java
@@ -0,0 +1,32 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class wordCountOccurance {
+
+	public static void main(String[] args) {
+String input = "My Name is my Name your";
+        
+// Convert the string to lowercase to make it case-insensitive
+input = input.toLowerCase();
+        
+// Split the string into words based on spaces
+String[] words = input.split("\\s+");
+
+ // Create a HashMap to store word counts
+Map<String, Integer> wordCountMap = new HashMap<>();
+
+// Count occurrences of each word
+for (String word : words) {
+    wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+}
+
+// Print the word counts
+System.out.println(wordCountMap);
+//for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+//     System.out.println(entry.getKey() + ": " + entry.getValue());
+//   }
+}
+
+}
